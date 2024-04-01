46a2bb295c38261f839b102c5045449325fd904c
SayantanAditya <sayantanaditya@gmail.com>
2024-04-02 01:27:29 +0530

    Almost complete

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { createTodo, updateTodo } = require('./types');
 const { todo } = require("./db");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.post("/todo", async function(req,res){
 
@@ -55,3 +57,4 @@ app.put("/completed",async function(req,res){
     })
 })
 
+app.listen(3000);
