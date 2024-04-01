a81d59af999611659b46738f30819b3f2a054e46
SayantanAditya <sayantanaditya@gmail.com>
2024-04-01 14:51:59 +0530

    backend-comp

---
+++ backend/db.js
@@ -0,0 +1,30 @@
+/*
+Todo {
+  title : string ;
+  description: string;
+  completed: boolean
+}
+
+*/
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
+const todoSchema = mongoose.Schema({
+  title: String,
+  description: String,
+  completed: Boolean
+});
+
+const todo = mongoose.model('todos', todoSchema);
+
+module.exports = {
+  todo
+};
